543783bbacae30a47c598ea72ee23bc4e8275697
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-06-15 13:15:32 -0500

    Used generator for User and Post models

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    content "MyString"
+    timestamp "2018-06-15 13:13:44"
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    nickname "MyString"
+    email "MyString"
+  end
+end
