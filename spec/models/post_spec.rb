322ed9c1cbe913056a170113cc591e6fcf2cd460
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-06-18 13:47:42 -0500

    Created test for liking posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user){FactoryBot.create(:user)}
+  let(:other){FactoryBot.create(:user)}
+  let(:post){FactoryBot.create(:post, user_id: user.id)}
+
+  it "Can be liked by a user" do
+    post.liked_by_users << other
+    expect(post.liked_by_users).to include(other)
+  end
+
 end
