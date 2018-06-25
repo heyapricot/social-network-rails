c6b4a09df8010d098bdc17541d7605c565b49048
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-06-25 15:21:45 -0500

    Finished customization of Login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: :index
   root to: 'home#index'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
