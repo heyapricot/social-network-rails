31582899dd9da51e6ead48c34c259737b74b16a2
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-06-28 11:06:44 -0500

    User has a show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: :index
+  resources :users, only: %i[show]
   root to: 'home#index'
 
   devise_scope :user do
