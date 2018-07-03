1ff54c57517ef1ce107f2b50d66b26b51ba43793
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-03 17:28:10 -0500

    Created pending test for new post on user's show page

--- spec/features/user_actions_spec.rb
+++ spec/features/user_actions_spec.rb
@@ -7,4 +7,14 @@ RSpec.feature "UserActions", type: :feature do
     visit user_path(user.id)
     expect(page).to have_text(user.first_name)
   end
+
+  pending "Create a post from the current user's show page" do
+    test_text = "This is a test"
+    visit user_path(user.id)
+    fill_in "post_content", with: test_text
+    click_button "Post"
+    save_and_open_page
+    expect(page).to have_text(test_text)
+  end
+
 end
