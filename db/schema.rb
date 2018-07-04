2ee704a0d9e47cfe5f6e5cac5aae7139752ffd06
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-04 17:11:35 -0500

    Created comment model and test in post spec  for comments (passed)

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20180712204302) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
