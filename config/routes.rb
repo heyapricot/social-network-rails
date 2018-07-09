fab2037c5f5dfbec86e0d2e0a13f9d8bdb4fa908
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-09 17:39:42 -0500

    Created route and view for user index page and turned profile header into a partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: :index
-  resources :users, only: %i[show] do
+  resources :users, only: %i[show index] do
     resources :posts, only: %i[create]
   end
   resources :posts, only: %i[create] do
