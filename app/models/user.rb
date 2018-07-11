997466600cbb1a69a3b8e5dec66d5f96c035c460
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-11 15:18:07 -0500

    Removed reference to friendship requests on User model and reference to timestamp on Post factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-  has_many :friendship_requests
   has_many :friendships
   has_many :posts
   has_many :views
   has_many :likes
 
-
-  has_many :requesters, :through => :friendship_requests, :class_name => "User", :foreign_key => "friend_id", :source => "friend"
   has_many :friends, :through => :friendships, :class_name => "User", :foreign_key => "friend_id"
   has_many :viewed_posts, :through => :views, :class_name => "Post", :foreign_key => "post_id", :source => "post"
   has_many :liked_posts, :through => :views, :class_name => "Post", :foreign_key => "post_id", :source => "post"
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :post do
     sequence(:content) {|n| "I've composed #{n} post"}
-    timestamp {DateTime.current}
   end
 end
