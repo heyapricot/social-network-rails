0782e2f3eed5be262b795d18cf04c353a157e46f
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-11 15:28:53 -0500

    Created and run migration to add status column to friendship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711181935) do
+ActiveRecord::Schema.define(version: 20180711202321) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
