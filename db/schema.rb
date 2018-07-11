26f7871caefef42ae12e088d4731ab9fdefd3413
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-11 13:21:03 -0500

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618210748) do
+ActiveRecord::Schema.define(version: 20180711181935) do
 
   create_table "friendship_requests", force: :cascade do |t|
     t.integer "user_id"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20180618210748) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
-    t.datetime "timestamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
