cc61d66b1ca6bb6509438d9ebac81275aefebf3d
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-13 14:58:10 -0500

    Improved post model testing after addition of PostActions model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,18 +2,55 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-  let(:user){FactoryBot.create(:user)}
-  let(:other){FactoryBot.create(:user)}
-  let(:post){FactoryBot.create(:post, user_id: user.id)}
+  let(:users){FactoryBot.create_list(:user, 2)}
+  let(:post){FactoryBot.create(:post, author: user)}
 
-  it "Can be liked by a user" do
-    post.likers << other
-    expect(post.likers).to include(other)
+  context "when liking a post" do
+    let(:users) {FactoryBot.create_list(:user, 4)}
+    let(:post){FactoryBot.create(:post, author: users.first)}
+
+    it "keeps track of the number of likes" do
+      users.each { |u| post.likes.create(user: u) }
+      expect(post.likes.size).to eq users.length
+    end
+
+    it "can get a list of Users who liked the post" do
+      users.each { |u| post.likes.create(user: u) }
+      expect(post.likers).to match_array(users)
+    end
   end
 
-  it "Can keep track of the users who viewed the post" do
-    post.viewers << other
-    expect(post.viewers).to include(other)
+  context "when viewing a post" do
+    let(:users) {FactoryBot.create_list(:user, 4)}
+    let(:post){FactoryBot.create(:post, author: users.first)}
+
+    it "keeps track of the number of views" do
+      users.each { |u| post.viewers << u }
+      expect(post.views.size).to eq users.length
+    end
+
+    it "can get a list of Users who viewed the post" do
+      users.each { |u| post.viewers << u }
+      expect(post.viewers).to match_array(users)
+    end
+  end
+
+  context "when commenting a post" do
+    let(:users) {FactoryBot.create_list(:user, 4)}
+    let(:post){FactoryBot.create(:post, author: users.first)}
+    content = "Test comment"
+    before {users.each { |u| post.comments.create(user: u, content: content) }}
+
+    it "keeps track of the number of comments" do
+      post.comments.each do |c|
+        expect(c.content).to eq content
+        expect(users.include? c.user).to be true
+      end
+    end
+
+    it "can get a list of Users who commented the post" do
+      expect(post.commenters).to match_array(users)
+    end
   end
 
 end
