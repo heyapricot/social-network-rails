9636e3b43cf67aa483e87942cf737c27304571ca
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-20 12:54:46 -0500

    Removed comment model

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: "User", foreign_key: "user_id"
-end
--- spec/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    content "MyString"
-    post nil
-    user nil
-  end
-end
