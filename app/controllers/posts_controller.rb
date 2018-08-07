0f4493e967fbba898425bef606d6193bb693e716
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-08-07 12:48:51 -0500

    Move the logic for optaining the posts to the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @posts = current_user.timeline_posts.order(created_at: :desc)
   end
 
   def create
