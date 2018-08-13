da0c83106111e57a6b6fedb14b1ef22d9242969d
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-08-13 15:54:33 -0500

    Added migration for user and corrected validation for avatar

---
+++ db/migrate/20180813202455_add_avatar_column_to_users.rb
@@ -0,0 +1,9 @@
+class AddAvatarColumnToUsers < ActiveRecord::Migration[5.1]
+  def self.up
+    add_attachment :users, :avatar
+  end
+
+  def self.down
+    remove_attachment :users, :avatar
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810215334) do
+ActiveRecord::Schema.define(version: 20180813202455) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -59,6 +59,10 @@ ActiveRecord::Schema.define(version: 20180810215334) do
     t.string "last_sign_in_ip"
     t.string "provider"
     t.string "uid"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
