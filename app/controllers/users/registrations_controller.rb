f602e8c15085b397a3bff6b056e7a44e5eb83e9a
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-08-13 16:42:05 -0500

    Added avatar to the devise aprameter sanitizer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname avatar])
   end
 
   # The path used after sign up.
